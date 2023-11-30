809eee9f6a7d0c8e101d1439d05509a20ee31dd9
David Balleza Ayala <davidballezaa@gmail.com>
2023-11-30 15:34:17 -0600

    Add Song component to display each song's data

---
+++ src/app/components/song.tsx
@@ -0,0 +1,19 @@
+interface SongProps {
+  image: string;
+  title: string;
+  artist: string;
+}
+
+const Song: React.FC<SongProps> = ({ image, title, artist }) => {
+  return (
+    <div className="song-container">
+      <img src={image} alt={`${title} album cover`} className="song-image" />
+      <div className="song-details">
+        <h2 className="song-title">{title}</h2>
+        <p className="song-name">{artist}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Song;

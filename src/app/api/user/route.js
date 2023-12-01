00dc6a142d562e3b8498c1af99c1eb0e2906d4ad
David Balleza Ayala <davidballezaa@gmail.com>
2023-12-01 00:11:07 -0600

    Refactor user route to use server-side authentication

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,7 +1,12 @@
 import { userInfo } from "../../../../lib/spotify";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]/route";
 
 export async function GET(request) {
-  const response = await userInfo();
+  const {
+    token: { accessToken },
+  } = await getServerSession(authOptions);
+  const response = await userInfo(accessToken);
 
   const data = await response.json();
 
